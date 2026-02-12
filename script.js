12465b5fe5a7791dc9916d5a62d4cec00ebd0604
Jackson tech <jacksonlumumba275@gmail.com>
2026-02-12 23:34:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const domains = [
+{
+name:"TechAfricaHub.com",
+price:"$450"
+},
+{
+name:"KenyaStartup.co.ke",
+price:"$200"
+}
+];
 
+const list = document.getElementById("domainList");
+
+domains.forEach(domain => {
+list.innerHTML += `
+<div class="card">
+<h3>${domain.name}</h3>
+<p>${domain.price}</p>
+<button>Buy</button>
+</div>
+`;
+});
