ef00d59abb2feb7913d548f804de9e7e9e7e9df2
Jackson tech <jacksonlumumba275@gmail.com>
2026-02-13 07:40:08 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,31 +1,17 @@
-// Firebase Core
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-
-// Firebase Services
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-storage.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
 
-
-// 🔥 YOUR PROJECT CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyDVzEfDFC02tzU74v7uw1hjEMTtUdNVHOQ",
   authDomain: "primedomains-africa.firebaseapp.com",
   projectId: "primedomains-africa",
   storageBucket: "primedomains-africa.firebasestorage.app",
   messagingSenderId: "186190235728",
-  appId: "1:186190235728:web:00fdbc0177cd579f7ce955",
-  measurementId: "G-WV8KCZQX73"
+  appId: "1:186190235728:web:00fdbc0177cd579f7ce955"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// Export Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
